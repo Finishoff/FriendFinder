d4726138b8bb093a5cafb735d48f9eb35a89793d
Finishoff <billvinhlam@gmail.com>
2019-01-17 15:28:36 -0800

    First commit

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+let friends = [
+    {
+      "name":"James",
+      "photo":"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      "scores":[
+          5,
+          4,
+          2,
+          5,
+          2
+        ] 
+    },
+    {
+      "name":"Kim",
+      "photo":"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      "scores":[
+          2,
+          4,
+          4,
+          4,
+          2
+        ]
+    },
+    {
+      "name":"Ash",
+      "photo":"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      "scores":[
+          1,
+          1,
+          1,
+          1,
+          1
+        ]
+    },
+    {
+      "name":"Sam",
+      "photo":"https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      "scores":[
+          5,
+          4,
+          3,
+          2,
+          1
+        ]
+    },
+    ];
+    
+    module.exports = friends;
+    
