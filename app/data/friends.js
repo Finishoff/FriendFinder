a50c9b7234c82a2ded1873eb79fa631383c900e4
Finishoff <billvinhlam@gmail.com>
2019-01-18 00:36:12 -0800

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,62 @@
 module.exports = [
     {
-      "name":"James",
-      "photo":"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      "scores":[
-          5,
-          4,
-          2,
-          5,
-          2
-        ] 
-    },
-    {
-      "name":"Kim",
-      "photo":"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      "scores":[
-          2,
-          4,
-          4,
-          4,
-          2
+        "name": "James",
+        "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores": [
+            5,
+            4,
+            2,
+            5,
+            2
         ]
     },
     {
-      "name":"Ash",
-      "photo":"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      "scores":[
-          1,
-          1,
-          1,
-          1,
-          1
+        "name": "Kim",
+        "photo": "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores": [
+            2,
+            4,
+            4,
+            4,
+            2
         ]
     },
     {
-      "name":"Sam",
-      "photo":"https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      "scores":[
-          5,
-          4,
-          3,
-          2,
-          1
+        "name": "Ash",
+        "photo": "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1
         ]
     },
-    ];
-    
+    {
+        "name": "Sam",
+        "photo": "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        "scores": [
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Frank",
+        "photo": "imgur.com/rwew3",
+        "scores": [
+            "2",
+            "1",
+            "2",
+            "1",
+            "3",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2"
+        ]
+    }
+]
